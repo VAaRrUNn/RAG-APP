4f4abbe9fdb6711e5498572eea809a590083a963
VaruN-dev-dev <sanatoo.varun666@gmail.com>
2024-07-01 18:44:12 +0530

    formatted main file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import faiss
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
@@ -9,29 +10,31 @@ import pprint
 warnings.filterwarnings("ignore")
 
 
-# making vector store
+def load_and_prepare_model():
+    documents = SimpleDirectoryReader("/content/data").load_data()
 
-# Load documents
-documents = SimpleDirectoryReader("/content/data").load_data()
+    embed_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2")
 
-# Create embedding model
-embed_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-mpnet-base-v2")
+    d = 768
+    faiss_index = faiss.IndexFlatL2(d)
 
-# Create vector store
-d = 768
-faiss_index = faiss.IndexFlatL2(d)
-# dimension should match your embedding model
-vector_store = FaissVectorStore(faiss_index=faiss_index)
+    vector_store = FaissVectorStore(faiss_index=faiss_index)
 
-index = VectorStoreIndex.from_documents(
-    documents,
-    embed_model=embed_model,
-    vector_store=vector_store
-)
+    index = VectorStoreIndex.from_documents(
+        documents,
+        embed_model=embed_model,
+        vector_store=vector_store
+    )
+
+    checkpoint = "microsoft/phi-1_5"
+    model = AutoModelForCausalLM.from_pretrained(checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+    return (index, model, tokenizer)
 
 
-def get_top_k_matches(query, k=3):
+def get_top_k_matches(query, index, k=3):
     """
     function to retrieve top k matches from vector store
     """
@@ -42,24 +45,6 @@ def get_top_k_matches(query, k=3):
 
 # Loading model and tokenizer
 
-checkpoint = "microsoft/phi-1_5"
-model = AutoModelForCausalLM.from_pretrained(checkpoint)
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
-
-# formatting prompt
-SYS_PROMPT = """You are an assistant for answering questions.
-You are given the extracted parts of a long document and a question. Provide a conversational answer.
-If you don't know the answer, just say "I do not know." Don't make up an answer."""
-
-
-def get_top_k_matches(query, k=3):
-    retriever = index.as_retriever(similarity_top_k=k)
-    nodes = retriever.retrieve(query)
-    # return nodes
-    return [node.node.text for node in nodes]
-
-
 def format_prompt(prompt, retrieved_docs, k):
     PROMPT = f"Question:{prompt}\nContext:"
     for text in retrieved_docs:
@@ -68,7 +53,7 @@ def format_prompt(prompt, retrieved_docs, k):
 
 
 # Generate function
-def generate(formatted_prompt):
+def generate(model, tokenizer, SYS_PROMPT, formatted_prompt):
     formatted_prompt = formatted_prompt[:2000]  # to avoid GPU OOM
     messages = [{"role": "system", "content": SYS_PROMPT},
                 {"role": "user", "content": formatted_prompt}]
@@ -91,10 +76,24 @@ def generate(formatted_prompt):
 
 
 def main():
-    query = input("Enter your query: ")
-    retrieved_docs = get_top_k_matches(query)
+    SYS_PROMPT = """You are an assistant for answering questions.
+    You are given the extracted parts of a long document and a question. Provide a conversational answer.
+    If you don't know the answer, just say "I do not know." Don't make up an answer."""
+
+    parser = argparse.ArgumentParser(
+        description="Simple CLI to take a string input")
+    parser.add_argument('-q', '--query', type=str,
+                        default="Tell me about y", help="Input query string")
+
+    args = parser.parse_args()
+    query = args.query
+
+    index, model, tokenizer = load_and_prepare_model()
+    retrieved_docs = get_top_k_matches(query, index)
+
     formatted_prompt = format_prompt(SYS_PROMPT, retrieved_docs, 3)
-    response = generate(formatted_prompt)
+    response = generate(model, tokenizer, SYS_PROMPT, formatted_prompt)
+
     pprint.pprint(response)
 
 
